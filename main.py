1f4fc70c9eb72c1ed9ef06a4a28f44481b28812f
teganasprey <82981420+teganasprey@users.noreply.github.com>
2023-08-08 11:22:14 -0400

    final minus grpahs

--- main.py
+++ main.py
@@ -6,51 +6,38 @@ import wave
 
 
 # Function to synthesize sine-waves for each band and superimpose them
-def synthesize_bands(filtered_chunks, rms_values, sampling_rate):
-    duration_per_chunk = len(filtered_chunks[0]) / sampling_rate
-    synthesized_chunks = []
-
-    for chunk_idx in range(len(filtered_chunks)//len(center_frequencies)):
-        t_chunk = np.linspace(0, duration_per_chunk, len(filtered_chunks[0]))
-        synthesized_chunk = np.zeros_like(filtered_chunks[0])
-
-        for i in range(len(center_frequencies)):
-            center_frequency = center_frequencies[i]
-            rms_value = rms_values[i]
-            amplitude = rms_value
-            t = t_chunk
-            band_wave = amplitude * np.sin(2 * np.pi * center_frequency * t)
-            synthesized_chunk += band_wave
-
-        synthesized_chunks.append(synthesized_chunk)
-
-    return synthesized_chunks
+def synthesize_chunk(chunk, rms, center_freq, sampling_rate, time):
+    times = np.arange(time, time + len(chunk)) / sampling_rate
+    band_wave = np.sin(2 * np.pi * center_freq * times)
+    amplitude = rms * band_wave
+    return amplitude
 
 
 # Function to apply band-pass filter
-def apply_bandpass_filter(audio_data, sampling_rate, center_frequency, bandwidth):
-    nyquist = 0.5 * sampling_rate
+def apply_bandpass_filter(chunk_data, sampling_rate, center_frequency, bandwidth):
     low = center_frequency - 0.5 * bandwidth
     high = center_frequency + 0.5 * bandwidth
-    low = low / nyquist
-    high = high / nyquist
-    b, a = signal.butter(5, [low, high], btype='band')
-    filtered_chunk = signal.lfilter(b, a, audio_data)
+    f = signal.butter(3,
+                 [low, high],
+                      btype='bandpass',
+                      output='sos',
+                      fs=sampling_rate)
+    filtered_chunk = signal.sosfilt(f, chunk_data)
     return filtered_chunk
 
 
 # Function for time segmentation
 def time_segmentation(audio_data, sampling_rate, chunk_size_ms):
     chunk_size_samples = int(sampling_rate * chunk_size_ms / 1000)
-    num_chunks = (len(audio_data) // chunk_size_samples) + 1
-    chunks = [audio_data[i*chunk_size_samples:(i+1)*chunk_size_samples] for i in range(num_chunks-1)]
-    if (num_chunks*chunk_size_samples)<len(audio_data):
-        chunks.append(audio_data[chunk_size_samples*num_chunks + 1:len(audio_data-1)])
+    num_chunks = len(audio_data) // chunk_size_samples
+    chunks = [audio_data[i*chunk_size_samples:(i+1)*chunk_size_samples] for i in range(num_chunks)]
+    if (num_chunks * chunk_size_samples) < len(audio_data):
+        chunks.append(audio_data[chunk_size_samples * num_chunks:-1])
     return chunks
 
 
 # file path
-PATH = '/Users/teganasprey/Desktop/'
+PATH = './'
 
 # whether to do plots
 DO_PLOTS = False
@@ -83,17 +70,25 @@ chunk_size_ms = 40
 chunks = time_segmentation(audio_data, sampling_rate, chunk_size_ms)
 
 # Define the center frequencies and bandwidths for the band-pass filters
-center_frequencies = [500, 1000, 2000, 4000, 8000]
+center_frequencies = [100+50*i for i in range(150)]
 bandwidth = 100
 
-# Apply band-pass filter to each chunk and store the filtered chunks
-filtered_chunks = [apply_bandpass_filter(chunk, sampling_rate, center_freq, bandwidth) for center_freq in center_frequencies for chunk in chunks]
-
-# Calculate the RMS value for each band
-rms_values = [np.sqrt(np.mean(chunk**2)) for chunk in filtered_chunks]
-
-# Synthesize the bands and superimpose them for each chunk
-synthesized_chunks = synthesize_bands(filtered_chunks, rms_values, sampling_rate)
+synthesized_chunks = []
+rms_values = []
+for chunk in chunks:
+    filter_chunks = []
+    for i, center_freq in enumerate(center_frequencies):
+        time = len(chunks[0]) * i
+        # Apply the band pass filter to the chunk
+        filtered_chunk = apply_bandpass_filter(chunk, sampling_rate, center_freq, bandwidth)
+        # Calculate the RMS of the filtered chunk
+        rms = np.sqrt(np.mean(filtered_chunk ** 2))
+        # For recording the RMS values for analysis
+        rms_values.append(rms)
+        # Synthesize the bands and superimpose them for each chunk
+        synthesized_chunk = synthesize_chunk(chunk, rms, center_freq, sampling_rate, time)
+        filter_chunks.append(synthesized_chunk)
+    synthesized_chunks.append(np.sum(filter_chunks, axis=0))
 
 # Concatenate the synthesized chunks to obtain the final output stream
 output_stream = np.concatenate(synthesized_chunks)
@@ -104,7 +99,8 @@ with wave.open(output_filename, 'wb') as wf:
     wf.setnchannels(1)  # Mono audio
     wf.setsampwidth(2)  # 2 bytes per sample (16-bit audio)
     wf.setframerate(sampling_rate)
-    wf.writeframes(output_stream.tobytes())
+    wf.setnframes(len(output_stream))
+    wf.writeframes(output_stream.astype(np.int16).tobytes())
 
 
 
